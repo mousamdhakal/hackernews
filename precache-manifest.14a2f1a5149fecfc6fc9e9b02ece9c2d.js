78c5a272d7c9d2ab2cad0b50b6ab43fed5a7ea45
mousamdhakal <mousamdhakal7.md@gmail.com>
2020-08-03 08:30:35 +0545

    Updates

---
+++ precache-manifest.14a2f1a5149fecfc6fc9e9b02ece9c2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afba0ff51659401d40c9f61048ef84ca",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "0ca41139c1d12431cf27",
+    "url": "/hackernews/static/css/main.0e5bca54.chunk.css"
+  },
+  {
+    "revision": "c09e1e693aa6cf340c8f",
+    "url": "/hackernews/static/js/2.e1b2c07e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hackernews/static/js/2.e1b2c07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca41139c1d12431cf27",
+    "url": "/hackernews/static/js/main.eeb5807d.chunk.js"
+  },
+  {
+    "revision": "c2b6706f847660835a7c",
+    "url": "/hackernews/static/js/runtime-main.0bf9feb2.js"
+  }
+]);
